4c7507daf42a7c13cf34d985693ebfb918fdce50
DanielLanghann <daniel.langhann@lector.ai>
2025-07-27 18:46:53 +0200

    Added datastore views and urls

--- datastore_api/urls.py
+++ datastore_api/urls.py
@@ -1,12 +1,11 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include 
 from . import views
 
 router = DefaultRouter()
+router.register(r"datastores", views.DatastoreViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
-    
+    path("", include(router.urls))
 ]
 
-app_name = "datastore_api"
